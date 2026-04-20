f351bb865f06cfcbd0b7c0da8c882eabb0845801
Saumya <saumya@gmail.com>
2026-04-20 15:38:05 +0530

    Hypothesis module complete

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from typing import Optional
 from data_loader import load_data
+from hypothesis_testing import run_tests
+from hypothesis_testing import get_all_states
 
 app = FastAPI()
 
@@ -15,6 +19,10 @@ app.add_middleware(
 
 df = load_data()
 
+@app.get("/states")
+def fetch_states():
+    return {"states": get_all_states()}
+
 
 @app.get("/")
 def home():
@@ -24,3 +32,15 @@ def home():
 @app.get("/data")
 def get_data():
     return df.to_dict(orient="records")
+
+@app.get("/hypothesis")
+def hypothesis(
+    state: Optional[str] = Query(None),
+    year_from: Optional[int] = Query(None),
+    year_to: Optional[int] = Query(None)
+):
+    return JSONResponse(run_tests(state, year_from, year_to))
+
+@app.get("/states")
+def get_states():
+    return sorted(df['State'].dropna().unique().tolist())
